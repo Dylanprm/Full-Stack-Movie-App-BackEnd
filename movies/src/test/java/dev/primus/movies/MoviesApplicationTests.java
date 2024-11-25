4ee7df5d51ad78bb73014bbf56378a11d4736387
Dylanprm <dylanprm@yahoo.com>
2024-11-25 17:56:43 -0500

    Initial commit

---
+++ movies/src/test/java/dev/primus/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.primus.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
